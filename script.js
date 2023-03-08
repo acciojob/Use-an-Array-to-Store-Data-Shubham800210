52b3d018169c97da17f1058dfd3d7aedf492000a
shubham kumar <65395298+Shubham800210@users.noreply.github.com>
2023-03-08 16:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-
-let yourArray = [];
-for(let i = 0; i < 5; i++){
-	let obj = {`str&{i + 1}`, i, true};
-	yourArray.push(obj);
-}
+let yourArray = ['str', 1, true, undefined, null];
 console.log(yourArray.length);
